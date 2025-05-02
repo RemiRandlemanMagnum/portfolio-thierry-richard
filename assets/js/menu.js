f99f7b63333237080e69ca35b74b84f5b3d3105c
RemiRandlemanMagnum <204841834+RemiRandlemanMagnum@users.noreply.github.com>
2025-05-02 23:53:39 +0200

    finalisation site

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,3 +1,10 @@
+window.addEventListener('load', () => {
+  const loader = document.getElementById('loader');
+  setTimeout(() => {
+    loader.classList.add('hidden');
+  }, 2300); // Laisse le temps à l’animation (2s)
+});
+
 const menuToggle = document.querySelector('.menu-toggle');
 const menu = document.getElementById('menu');
 const closeBtn = document.querySelector('.menu-close');
@@ -12,4 +19,27 @@ closeBtn.addEventListener('click', () => {
   menu.classList.remove('open');
   menuToggle.setAttribute('aria-expanded', 'false');
   menu.setAttribute('aria-hidden', 'true');
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const signature = document.querySelector('.signature-geante');
+  const contactSection = document.querySelector('#contact');
+  
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        signature.style.transition = 'background-size 2s ease';
+        signature.style.backgroundSize = '100% 100%';
+      } else {
+        signature.style.transition = 'background-size 0.8s ease-out';
+        signature.style.backgroundSize = '0% 100%';
+      }
+    });
+  }, {
+    threshold: 0.8
+  });
+
+  if (contactSection) {
+    observer.observe(contactSection);
+  }
 });
